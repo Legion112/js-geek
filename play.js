8eca198ab86ce3d440a8ac5903e2045aae9b9b96
Max <ageev.maks2014@gmail.com>
2019-02-15 19:54:12 +0500

    06-const

--- play.js
+++ play.js
@@ -1,13 +1,15 @@
-console.log(aGreatNumber);
+const aGreatNumber = 10;
+const aGreatObject = {withGreatKey: true};
 
-let aGreatNumber = 10;
+aGreatObject.withGreatKey = false;
 
 if (true) {
-    aGreatNumber = 42;
+    //aGreatNumber = 42;
 }
 
 setTimeout(() => {
     console.log(aGreatNumber);
+    console.log(aGreatObject);
 }, 1000);
 
 console.log('waiting...');
--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -37,7 +37,7 @@
         },
         handleRepLogDelete: function (e) {
             e.preventDefault();
-            let $link = $(e.currentTarget);
+            const $link = $(e.currentTarget);
             swal({
                 title: 'Delete this log?',
                 text: 'What? Did you not actually lift this?',
@@ -54,8 +54,8 @@
                 .removeClass('fa-trash')
                 .addClass('fa-spinner')
                 .addClass('fa-spin');
-            let deleteUrl = $link.data('url');
-            let $row = $link.closest('tr');
+            const deleteUrl = $link.data('url');
+            const $row = $link.closest('tr');
             return $.ajax({
                 url: deleteUrl,
                 method: 'DELETE',
@@ -77,8 +77,8 @@
         handleNewFormSubmit: function (e) {
             e.preventDefault();
 
-            let $form = $(e.currentTarget);
-            let formData = {};
+            const $form = $(e.currentTarget);
+            const formData = {};
             $.each($form.serializeArray(), (key, fieldData) => {
                 formData[fieldData.name] = fieldData.value;
             });
@@ -104,7 +104,7 @@
                         resolve(data);
                     })
                 }).catch(jqXHR => {
-                    let errorData = JSON.parse(jqXHR.responseText);
+                    const errorData = JSON.parse(jqXHR.responseText);
                     reject(errorData);
                 });
             });
@@ -113,35 +113,35 @@
         _mapErrorsToForm: function (errorData) {
             // reset things
             this._removeFormErrors();
-            let $form = this.$wrapper.find(this._selectors.newRepForm);
+            const $form = this.$wrapper.find(this._selectors.newRepForm);
 
             $form.find(':input').each((index, element) => {
-                let fieldName = $(element).attr('name');
-                let $wrapper = $(element).closest('.form-group');
+                const fieldName = $(element).attr('name');
+                const $wrapper = $(element).closest('.form-group');
                 if (!errorData[fieldName]) {
                     // no error!
                     return;
                 }
-                let $error = $('<span class="js-field-error help-block"></span>');
+                const $error = $('<span class="js-field-error help-block"></span>');
                 $error.html(errorData[fieldName]);
                 $wrapper.append($error);
                 $wrapper.addClass('has-error');
             });
         },
         _removeFormErrors: function () {
-            let $form = this.$wrapper.find(this._selectors.newRepForm);
+            const $form = this.$wrapper.find(this._selectors.newRepForm);
             $form.find('.js-field-error').remove();
             $form.find('.form-group').removeClass('has-error');
         },
         _clearForm: function () {
             this._removeFormErrors();
-            let $form = this.$wrapper.find(this._selectors.newRepForm);
+            const $form = this.$wrapper.find(this._selectors.newRepForm);
             $form[0].reset();
         },
         _addRow: function (repLog) {
-            let tplText = $('#js-rep-log-row-template').html();
-            let tpl = _.template(tplText)
-            let html = tpl(repLog)
+            const tplText = $('#js-rep-log-row-template').html();
+            const tpl = _.template(tplText)
+            const html = tpl(repLog)
             this.$wrapper.find('tbody')
                 .append($.parseHTML(html))
             this.updateTotalWeightLifted();
@@ -150,7 +150,7 @@
     /**
      * A "private" object
      */
-    let Helper = function ($wrapper) {
+    const Helper = function ($wrapper) {
         this.$wrapper = $wrapper;
     };
     $.extend(Helper.prototype, {
